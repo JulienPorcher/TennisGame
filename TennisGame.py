156c22854eadd8df64e1693ce8ce3ef7b93718f6
JulienPorcher <julien.g.porcher@gmail.com>
2020-07-22 10:35:55 +0200

    Correction de la fonction Score() Remise à zero du score une fois le jeu gagné

--- TennisGame.py
+++ TennisGame.py
@@ -13,6 +13,7 @@ class TennisGame():
         self.player_2 = player_2
         self.score = [0,0]
         self.dict_score = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+        self.morethan3 = False
 
 
     def Point(self, player):
@@ -34,9 +35,11 @@ class TennisGame():
     def Asadvantage(self):
 
         if self.score[0] >= 4 and self.score[0]-self.score[1] == 1:
+            self.morethan3 = True
             print("Advantage " + self.player_1)
 
         elif self.score[1] >= 4 and self.score[1]-self.score[0] == 1:
+            self.morethan3 = True
             print("Advantage " + self.player_2)
         else:
             pass
@@ -45,9 +48,15 @@ class TennisGame():
     def Game(self):
 
         if self.score[0] >= 4 and self.score[0]-self.score[1] >= 2:
+            self.morethan3 = True
+            self.score = [0,0]
             print("Game " + self.player_1)
+            
+
         elif self.score[1] >= 4 and self.score[1]-self.score[0] >= 2:
-            print("Game " + self.player_1)
+            self.morethan3 = True
+            self.score = [0,0]
+            print("Game " + self.player_2)
             
 
     def Score(self):
@@ -58,6 +67,7 @@ class TennisGame():
 
         self.Game()
 
-        score_p1 = self.dict_score[self.score[0]]
-        score_p2 = self.dict_score[self.score[1]]
-        print(score_p1 + " - " + score_p2)
+        if self.morethan3 == False:
+            score_p1 = self.dict_score[self.score[0]]
+            score_p2 = self.dict_score[self.score[1]]
+            print(score_p1 + " - " + score_p2)
